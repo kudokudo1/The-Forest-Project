cf744bf19a46e3f593a59380c71248e55035cb57
Kudo ig <maple@kudostarstudios.com>
2026-09-23 01:06:58 -0400

    Promote Bristlecone Source Context: source_context/__init__.py

---
+++ recovery/bristlecone/implementation/source-context/source_context/__init__.py
@@ -0,0 +1,19 @@
+\
+from .model import (
+    SOURCE_CONTEXT_SCHEMA_VERSION,
+    SourceContextModelError,
+    SourceIdentity,
+    SourceFingerprint,
+    SourceRegion,
+    SourceTarget,
+)
+
+
+__all__ = (
+    "SOURCE_CONTEXT_SCHEMA_VERSION",
+    "SourceContextModelError",
+    "SourceIdentity",
+    "SourceFingerprint",
+    "SourceRegion",
+    "SourceTarget",
+)
